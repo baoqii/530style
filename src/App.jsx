f2154ce9ef7d6ed5cb03c5d1c31b220a5d82e5aa
Bao Qi <104255685+baoqii@users.noreply.github.com>
2023-09-09 10:29:08 +0800

    Add AnimatePresence and scrollToTop functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,58 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { Route, Routes, useLocation, BrowserRouter } from "react-router-dom";
 import "./App.css";
-
 import Shop from "./pages/Shop";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 import { ProductDetail } from "./pages/ProductDetail";
 import Layout from "./pages/Layout";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useRef } from "react";
 
-function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="*" element={<Error />} />
-      </Route>
-    )
-  );
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+  });
 
   return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home firstRender={firstRender} />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <AnimatedRoutes />
+    </BrowserRouter>
   );
 }
 
